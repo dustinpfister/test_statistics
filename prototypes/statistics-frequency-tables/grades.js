0b3e444dd65acc790bae94d5af2f990f2fb0bcf1
dustinpfister <dustin.pfister@gmail.com>
2020-04-20 15:20:27 -0400

    return array of sorted stat objects

--- prototypes/statistics-frequency-tables/grades.js
+++ prototypes/statistics-frequency-tables/grades.js
@@ -4,7 +4,22 @@ var makeFreqTable = function (grades) {
     grades.forEach(function (grade) {
         table[grade] = table[grade] === undefined ? 1 : table[grade] += 1;
     });
-    return table;
+    // return array of stat objects with grade
+    // and count properties, and sort by count
+    return Object.keys(table).map(function (key) {
+        return {
+            grade: key,
+            count: table[key]
+        }
+    }).sort(function (a, b) {
+        if (a.count > b.count) {
+            return -1;
+        }
+        if (a.count < b.count) {
+            return 1;
+        }
+        return 0;
+    });
 };
 
 var grades = ['c', 'a', 'f', 'f', 'b', 'c', 'f', 'a', 'f', 'd'];
