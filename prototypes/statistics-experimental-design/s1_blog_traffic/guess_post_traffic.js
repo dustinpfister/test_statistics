f6147ea3eb37cc873e2361afc17e8b8f0014a163
dustinpfister <dustin.pfister@gmail.com>
2021-03-01 09:53:00 -0500

    guess post traffic method looks good

--- prototypes/statistics-experimental-design/s1_blog_traffic/guess_post_traffic.js
+++ prototypes/statistics-experimental-design/s1_blog_traffic/guess_post_traffic.js
@@ -1,10 +1,14 @@
 
 // WC = Word Count
-// CPTW = Clicks Per Thousand Words
+// CPTW = Clicks Per Thousand Words Per Day
 // days = one 24 hour day
 
-let guessPostTraffic = (WC, CPTW, days) => {
-    wordCount = wordCount === undefined ? 500 : wordCount;
-    days = days === undefined ? days : 1;
+let guessPostTraffic = (WC, CPTWPD, days) => {
+    WC = WC === undefined ? 500 : WC;
+    CPTWPD = CPTWPD === undefined ? 1 : CPTWPD;
+    days = days === undefined ? 1 : days;
+    return WC * (CPTWPD / 1000) * days;
 };
 
+console.log( guessPostTraffic(1000, 1, 7) ); // 7
+console.log( guessPostTraffic(300, 120, 31) ); // 1116
