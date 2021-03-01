5d94c969eb02c5f1a6a2d33ad16d8c205ddba4b3
dustinpfister <dustin.pfister@gmail.com>
2021-03-01 11:51:35 -0500

    commnets

--- prototypes/statistics-experimental-design/s1_blog_traffic/get_post_traffic.js
+++ prototypes/statistics-experimental-design/s1_blog_traffic/get_post_traffic.js
@@ -14,7 +14,7 @@ let getPostTraffic = (WC, CPTWPD, days) => {
 console.log( getPostTraffic(1000, 1, 7) );   // 7
 console.log( getPostTraffic(300, 120, 31) ); // 1116
 
-// lodash includes is mu best post as of this writing
+// BEST POST => lodash includes is my best post as of this writing
 let bestPost = {
   wc: 875,
   CPTWPD: 23.755102040816325,
@@ -25,7 +25,8 @@ bestPost.traffic = getPostTraffic(bestPost.wc, bestPost.CPTWPD, bestPost.days);
 console.log( 'best post: ', bestPost.traffic );
 // best post: 582
 
-// site wide
+// SITE WIDE => site wide I have a grand total of 702,332 words over 813 posts
+// with a CPTWPD of only 0.9153180303006881
 let siteWide = {
   wc: 702332,
   CPTWPD: 0.9153180303006881,
@@ -36,5 +37,8 @@ siteWide.traffic = getPostTraffic(siteWide.wc, siteWide.CPTWPD, siteWide.days);
 console.log('site wide: ', siteWide.traffic);
 // site wide: 18000
 
-
+// My best preforming post is pulling in 582 clicks per 28 day span
+// which is way better than what is going on site wide which seems to
+// be ranking in around 22 per 28 day span
 console.log(siteWide.traffic / 813);
+// 22.14022140221402
