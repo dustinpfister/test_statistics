09015507b6fde2ac88ec174c8ac6e2b5f2df35c4
dustinpfister <dustin.pfister@gmail.com>
2021-03-01 09:41:20 -0500

    started a guess_post_traffic file

---
+++ prototypes/statistics-experimental-design/s1_blog_traffic/guess_post_traffic.js
@@ -0,0 +1,10 @@
+
+// WC = Word Count
+// CPTW = Clicks Per Thousand Words
+// days = one 24 hour day
+
+let guessPostTraffic = (WC, CPTW, days) => {
+    wordCount = wordCount === undefined ? 500 : wordCount;
+    days = days === undefined ? days : 1;
+};
+
