0ccd97d140801a49b2f4e8b081b912565ae4d188
dustinpfister <dustin.pfister@gmail.com>
2021-03-01 13:53:16 -0500

    some additional real examples

--- forpost/statistics-experimental-design/s1_basic_tools/get_CPTWPD.js
+++ forpost/statistics-experimental-design/s1_basic_tools/get_CPTWPD.js
@@ -8,14 +8,32 @@ let getCPTWPD = (WC, traffic, days) => {
     days = days === undefined ? 1 : days;
     return traffic / days / WC * 1000;
 };
+
+// a post that is 1,000 words that gets 7 clicks in one week
+// has a CPTWPD of 1
 console.log( getCPTWPD(1000, 7, 7) );     // 1
+// a post that is only 300 words that gets 1,116 clicks in a 31 day span
+// has a CPTWPD of 120
 console.log( getCPTWPD(300, 1116, 31) );  // 120
 
-// lodash includes
+// PLUG IN SOME REAL DATA FROM SEARCH CONSOLE
+
+// lodash includes is my best post as of this writing
 console.log('best post: ', getCPTWPD(875, 582, 28) );
 // 23.755102040816325
 
+// I can also plug in a site wide total for word count along with
+// traffic for a 28 day span to get this CPTWPD value for thw whole site
 let siteWideTotalWC = 702332;
 let siteWide = getCPTWPD(siteWideTotalWC, 18000, 28);
 console.log('site wide: ', siteWide);
 // 0.9153180303006881
+
+// some posts that are 1,800+ words
+console.log('js-array-length: ', getCPTWPD(2777, 160, 28) );      // 2.057719018468028
+console.log('js-javascript-foreach: ', getCPTWPD(3805, 29, 28) ); // 0.27219823540454297
+
+// some posts that are < 1000 words
+console.log('lodash_includes : ', getCPTWPD(875, 582, 28) ); // 23.755102040816325
+console.log('lodash_groupby : ', getCPTWPD(856, 549, 28) ); // 22.905540720961284
+console.log('lodash_sum : ', getCPTWPD(800, 502, 28) ); // 22.410714285714285
