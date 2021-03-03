24c58d987edc199727a6cd2626eead1a8c969125
dustinpfister <dustin.pfister@gmail.com>
2021-03-03 16:20:10 -0500

    commnets

--- forpost/statistics-stochastic-process-bernoulli/s3_pure.js
+++ forpost/statistics-stochastic-process-bernoulli/s3_pure.js
@@ -1,9 +1,9 @@
 
 var coin = function(index){
-    return index % 2;
+    return (index || 0) % 2;
 };
 
-console.log( coin(0) );
-console.log( coin(1) );
-console.log( coin(2) );
-console.log( coin(3) );
+console.log( coin(0) ); // 0
+console.log( coin(1) ); // 1
+console.log( coin(2) ); // 0
+console.log( coin(3) ); // 1
