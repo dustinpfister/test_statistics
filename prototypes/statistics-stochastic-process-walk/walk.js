794858573aea9877009848414ae43b604ac48c17
dustinpfister <dustin.pfister@gmail.com>
2021-03-03 15:29:37 -0500

    okay step method working

--- prototypes/statistics-stochastic-process-walk/walk.js
+++ prototypes/statistics-stochastic-process-walk/walk.js
@@ -1,7 +1,7 @@
 
-var dirs = [0, 1.57]
+var dirs = [0, Math.PI * 0.5, Math.PI, Math.PI * 1.5];
 var step = function(){
-   var dir = dirs[Math.floor(Math.random() * dirs.length)];
+   var dir = dirs[3]; //dirs[Math.floor(Math.random() * dirs.length)];
    return {
        x: Math.round(Math.cos(dir)),
        y: Math.round(Math.sin(dir))
