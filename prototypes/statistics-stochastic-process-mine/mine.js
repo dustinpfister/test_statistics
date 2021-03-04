830b85ad76e93f7632274fdac8be6fb0f496c71a
dustinpfister <dustin.pfister@gmail.com>
2021-03-04 09:03:53 -0500

    single random method working

--- prototypes/statistics-stochastic-process-mine/mine.js
+++ prototypes/statistics-stochastic-process-mine/mine.js
@@ -1,18 +1,27 @@
 
-var ores = [
+var defaultOres = [
     { type: 'iron', chance: 1 },
-    { type: 'copper': chance: 0.1 },
-    { type: 'gold': chance: 0.01 }
+    { type: 'copper', chance: 0.1 },
+    { type: 'gold', chance: 0.01 }
 ];
 
+var mineMethods = {
+    singleRandom: function(ore, count){
+        return Math.floor( Math.random() * ore.chance );
+    }
+};
 
-var mine = function(ores, count){
+var mine = function(ores, count, mineMethod){
+    ores = ores === undefined ? defaultOres : ores;
     count = count === undefined ? 1 : count;
+    mineMethod = mineMethod === undefined ? mineMethods.singleRandom : mineMethod;
     return ores.map(function(oreObj){
-        
+        var amount = mineMethod(oreObj, count);
         return {
             type: oreObj.type,
-            count: count
+            amount: amount
         };
     });
 };
+
+console.log( mine() );
