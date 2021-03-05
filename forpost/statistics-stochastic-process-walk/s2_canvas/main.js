c57d1dc9330f7a6c06ed7fe3266fbbeb089e68ce
dustinpfister <dustin.pfister@gmail.com>
2021-03-05 12:22:02 -0500

    main app loop is click driven for now

--- forpost/statistics-stochastic-process-walk/s2_canvas/main.js
+++ forpost/statistics-stochastic-process-walk/s2_canvas/main.js
@@ -1,25 +1,32 @@
-var state = {
-    canvasObj : utils.createCanvas({
-        width: 640,
-        height: 480
-    }),
-    grid: grid.create(),
-    pool: poolMod.create()
-};
-
-// walk object(s)
-var obj = state.pool.objects[0];
-var delta = walk(obj, state.grid, state.pool);
-
-obj.x += delta.x;
-obj.y += delta.y;
-
-// draw
-var ctx = state.canvasObj.ctx;
-draw.background(ctx, state);
-draw.grid(ctx, state);
-draw.pool(ctx, state);
-
+(function(){
+    // main app state
+    var state = {
+        canvasObj : utils.createCanvas({
+            width: 640,
+            height: 480
+        }),
+        grid: grid.create(),
+        pool: poolMod.create()
+    };
+    // app loop
+    var loop = function(){
+        // walk object(s)
+        var obj = state.pool.objects[0];
+        var delta = walk(obj, state.grid, state.pool);
+        obj.x += delta.x;
+        obj.y += delta.y;
+        // draw
+        var ctx = state.canvasObj.ctx;
+        draw.background(ctx, state);
+        draw.grid(ctx, state);
+        draw.pool(ctx, state);
+    };
+    loop();
+    // click canvas to call loop
+    state.canvasObj.canvas.addEventListener('click', function(e){
+    loop();
+    });
+}());
 // random deltas
 // console.log(walk());
 // always 4 ( { x: -1, y: 0 } )
