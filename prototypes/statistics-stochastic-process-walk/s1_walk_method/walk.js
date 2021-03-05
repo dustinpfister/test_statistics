23fcc99a0bb63c912fc015af50c3d7d65aa9cf25
dustinpfister <dustin.pfister@gmail.com>
2021-03-05 09:36:05 -0500

    walk.js module

--- prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
+++ prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
@@ -1,18 +1,21 @@
-
-var dirs = [0, Math.PI * 0.5, Math.PI, Math.PI * 1.5];
-
-var stepMethods = {
-    random: function(){
-        return dirs[Math.floor(Math.random() * dirs.length)];
-    }
-};
-
-var step = function(method){
-   var dir = method === undefined ? stepMethods.random(): method(); 
-   return {
-       x: Math.round(Math.cos(dir)),
-       y: Math.round(Math.sin(dir))
-   };
-};
-
-console.log(step())
+var walk = (function(){
+    // default dirs
+    var dirs = [0, Math.PI * 0.5, Math.PI, Math.PI * 1.5];
+    // built in step methods
+    var stepMethods = {
+        random: function(){
+            return dirs[Math.floor(Math.random() * dirs.length)];
+        }
+    };
+    // Public API
+    var api = function(method){
+        var dir = method === undefined ? stepMethods.random(): method(); 
+        return {
+            x: Math.round(Math.cos(dir)),
+            y: Math.round(Math.sin(dir))
+        };
+    };
+    // return public API
+    return api;
+}());
+console.log(walk());
