b93f0d1e3fbb0b6a524194db96876ae1f287668f
dustinpfister <dustin.pfister@gmail.com>
2021-03-05 11:35:08 -0500

    create canvas

--- forpost/statistics-stochastic-process-walk/s2_canvas/main.js
+++ forpost/statistics-stochastic-process-walk/s2_canvas/main.js
@@ -1,4 +1,27 @@
 
+var draw = (function(){
+    var api = {};
+    // draw background
+    api.background = function(ctx, state, style){
+        var canvas = state.canvasObj.canvas;
+        ctx.fillStyle = style || 'black';
+        ctx.fillRect(0,0, canvas.width, canvas.height);
+    };
+    // return public api
+    return api;
+}());
+
+var state = {
+    canvasObj : utils.createCanvas({
+        width: 640,
+        height: 480
+    })
+};
+
+// draw
+var ctx = state.canvasObj.ctx;
+draw.background(ctx, state);
+
 // random deltas
 console.log(walk());
 // always 4 ( { x: -1, y: 0 } )
