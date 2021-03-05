b1fc8364e692eae9df2e4ea2024f4d8648e063bb
dustinpfister <dustin.pfister@gmail.com>
2021-03-05 09:46:15 -0500

    clean up

--- prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
+++ prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
@@ -1,12 +1,9 @@
 var walk = (function(){
-    var dirs = [0, Math.PI * 0.5, Math.PI, Math.PI * 1.5];
-
     // default dir movement array
     // [0,1,2,3,4,5,6,7] => 8 dir movement
     // [0,2,4,6];        => 4 dir movement
     // [6];              => always go up
     var default_dirs = [0,2,4,6];
-
     // built in step methods
     var stepMethods = {
         random: function(dirs){
@@ -18,7 +15,9 @@ var walk = (function(){
         dirs = dirs === undefined ? default_dirs : dirs;
         // call step method and get a d num (0-7)
         var d = method === undefined ? stepMethods.random(dirs): method(dirs);
+        // convert to radian
         var radian = Math.PI * 2 / 8 * d;
+        // use Sin And Cos to return delta values for x and y
         return {
             x: Math.round(Math.cos(radian)),
             y: Math.round(Math.sin(radian))
