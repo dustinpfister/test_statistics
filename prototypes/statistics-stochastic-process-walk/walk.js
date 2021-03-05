8876a538929acd83746b9ec8e227cb806d25713e
dustinpfister <dustin.pfister@gmail.com>
2021-03-05 09:32:07 -0500

    step method object started

--- prototypes/statistics-stochastic-process-walk/walk.js
+++ prototypes/statistics-stochastic-process-walk/walk.js
@@ -1,7 +1,14 @@
 
 var dirs = [0, Math.PI * 0.5, Math.PI, Math.PI * 1.5];
-var step = function(){
-   var dir = dirs[3]; //dirs[Math.floor(Math.random() * dirs.length)];
+
+var stepMethods = {
+    random: function(){
+        return dirs[Math.floor(Math.random() * dirs.length)];
+    }
+};
+
+var step = function(method){
+   var dir = method === undefined ? stepMethods.random(): method(); 
    return {
        x: Math.round(Math.cos(dir)),
        y: Math.round(Math.sin(dir))
