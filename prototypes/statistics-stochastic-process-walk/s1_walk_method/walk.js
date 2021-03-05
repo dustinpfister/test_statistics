a161f7d40a7d4f6332e5d002b467470491753bcc
dustinpfister <dustin.pfister@gmail.com>
2021-03-05 09:55:42 -0500

    fixed simple bug with radian value

--- prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
+++ prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
@@ -4,27 +4,36 @@ var walk = (function(){
     // [0,2,4,6];        => 4 dir movement
     // [6];              => always go up
     var default_dirs = [0,2,4,6];
-    // built in step methods
-    var stepMethods = {
-        random: function(dirs){
+    // built in walk methods
+    var walkMethods = {
+        // random dir
+        rnd: function(obj, dirs){
             return dirs[Math.floor(Math.random() * dirs.length)];
+        },
+        // use a heading prop of an object, or default to 0
+        useHeading: function(obj, dirs){
+            return obj.heading || 0;
         }
     };
     // Public API
-    var api = function(method, dirs){
+    var api = function(obj, method, dirs){
+        obj = obj || {x: 0, y: 0};
         dirs = dirs === undefined ? default_dirs : dirs;
         // call step method and get a d num (0-7)
-        var d = method === undefined ? stepMethods.random(dirs): method(dirs);
+        var d = method === undefined ? walkMethods.rnd(obj, dirs): method(obj, dirs);
         // convert to radian
-        var radian = Math.PI * 2 / 8 * d;
+        var radian = Math.PI * 2 / (dirs.length) * d;
         // use Sin And Cos to return delta values for x and y
         return {
             x: Math.round(Math.cos(radian)),
             y: Math.round(Math.sin(radian))
         };
     };
+    // make walk methods public
+    api.wm = walkMethods;
     // return public API
     return api;
 }());
 
-console.log(walk());
+console.log( walk({x:0, y:0, heading:1}, walk.wm.useHeading) );
+
