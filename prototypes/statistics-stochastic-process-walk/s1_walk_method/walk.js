9e1495ab556c9241469699a1f1a0517b400364f5
dustinpfister <dustin.pfister@gmail.com>
2021-03-05 09:58:05 -0500

    commnets

--- prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
+++ prototypes/statistics-stochastic-process-walk/s1_walk_method/walk.js
@@ -35,5 +35,7 @@ var walk = (function(){
     return api;
 }());
 
-console.log( walk({x:0, y:0, heading:1}, walk.wm.useHeading) );
-
+// random deltas
+console.log(walk());
+// always 4 ( { x: -1, y: 0 } )
+console.log( walk({x:0, y:0, heading:1}, walk.wm.useHeading, [0, 4]) );
